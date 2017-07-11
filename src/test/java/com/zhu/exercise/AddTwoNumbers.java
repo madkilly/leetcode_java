4f0443d4f4d63c809f1e7a72788940e8ce0c8859
zhukai1 <zhukai1@yonyou.com>
2017-07-11 20:16:04 +0800

    1

--- src/test/java/com/zhu/exercise/AddTwoNumbers.java
+++ src/test/java/com/zhu/exercise/AddTwoNumbers.java
@@ -69,6 +69,30 @@ public class AddTwoNumbers {
 		return result;
 	}
 	
+	 public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+	        ListNode c1 = l1;
+	        ListNode c2 = l2;
+	        ListNode sentinel = new ListNode(0);
+	        ListNode d = sentinel;
+	        int sum = 0;
+	        while (c1 != null || c2 != null) {
+	            sum /= 10;
+	            if (c1 != null) {
+	                sum += c1.val;
+	                c1 = c1.next;
+	            }
+	            if (c2 != null) {
+	                sum += c2.val;
+	                c2 = c2.next;
+	            }
+	            d.next = new ListNode(sum % 10);
+	            d = d.next;
+	        }
+	        if (sum / 10 == 1)
+	            d.next = new ListNode(1);
+	        return sentinel.next;
+	    }
+	
 	public ListNode genLinkList(int [] group){
 		ListNode result = new ListNode(0);
 		ListNode tmp = result;
