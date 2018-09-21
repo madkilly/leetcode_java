f6ba1034ce1c53441eb1e4ab24fde89afe2839b5
zhukai1 <zhukai1@yonyou.com>
2018-09-21 16:12:01 +0800

    fix

--- details/ReverseInt/ReverseInt.md
+++ details/ReverseInt/ReverseInt.md
@@ -18,3 +18,23 @@ Note:
 Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
 拿到题目第一个解决方案:就是队列 先进先出
+
+# 总结
+
+## 数据溢出问题
+
+	Integer.MIN_VALUE = -2147483648
+	Integer.MAX_VALUE = 2147483647
+	Long.MIN_VALUE = -9223372036854775808
+	Long.MAX_VALUE = 9223372036854775807
+	Float.MIN_VALUE = 1.4E-45
+	Float.MIN_NORMAL = 1.17549435E-38
+	Float.MAX_VALUE = 3.4028235E38
+	Double.MAX_VALUE = 1.7976931348623157E308
+	Double.MIN_VALUE = 4.9E-324
+
+java int最大最小值 是有上限的 这个问题一开始没有考虑到，其实题目也有提到溢出返回0.这块是自己粗心了
+
+## 效率问题
+	
+我一开始采用了队列处理，走了两个循环效率比较低，其实可以采用单循环一次算出结果的
--- src/test/java/com/zhu/exercise/ReverseInt.java
+++ src/test/java/com/zhu/exercise/ReverseInt.java
@@ -7,34 +7,69 @@ import org.junit.Test;
 
 public class ReverseInt {
 
-	public int getReverseInt(int raw){
+	public int getReverseInt(int raw_input){
+		Long raw = new Long(raw_input);
 		boolean positive_flag=true;
 		if(raw<0){
 			raw=-raw;
 			positive_flag=false;
 		}
-		Queue<Integer> tq = new LinkedList<Integer>();
-		Integer tmp_raw= raw;
+		Queue<Long> tq = new LinkedList<Long>();
+		Long tmp_raw= raw;
 		while(tmp_raw!=0){
-			Integer mod = tmp_raw%10;
+			Long mod = tmp_raw%10;
 			tmp_raw = tmp_raw/10;
 			tq.add(mod);
 			
 		}
-		Integer result=0;
+		Long result=(long) 0;
 		while(!tq.isEmpty()){
 			result*=10;
 			result+=tq.poll();
 		}
+		if(result>Integer.MAX_VALUE) {
+			result=(long) 0;
+		}
 		if(!positive_flag){
 			result=-result;
 		}
-		return result;
+		return result.intValue();
 	}
+	
+	
+	/**
+	 * @param x
+	 * @return
+	 * 改成一次循环
+	 */
+	public int enhanceReverse(int x) {
+		boolean negative_flag = false;
+		long tmp = 0;
+		if(x<0) {
+			negative_flag=true;
+			x-=x;
+		}
+		
+		while(x!=0) {
+			tmp=tmp*10;
+			tmp+=x%10;
+			x/=10;
+		}
+		if(tmp >Integer.MAX_VALUE) {
+			return 0;
+		}else if(negative_flag){
+			tmp=-tmp;
+			return (int) tmp;
+		}
+		return (int)tmp;
+	}
+	
 	@Test
 	public void testreserve(){
-		int result = getReverseInt(12345);
+		int result = getReverseInt(634);
+		int result2 = enhanceReverse(4561351);
 		System.out.println(result);
+		System.out.println(result2);
 	}
 	
 }
